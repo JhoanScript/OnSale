9e77dd31d33b0d5c04b01dd64aa45c23a6d20dcb
JhoanScript <jhoan.hidrogo@gmail.com>
2024-05-25 20:33:50 -0400

    Customize controllers, models, and product views

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+    has_one_attached :photo
+
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
+end
