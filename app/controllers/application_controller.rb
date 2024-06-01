eca2cb01a29df5d03f336435e5cb8b896d77e32b
JhoanScript <jhoan.hidrogo@gmail.com>
2024-06-01 12:06:26 -0400

    Update products controllers and views to begin internationalization: application_controller.rb, products_controller.rb, view files and config/locales/en.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    around_action :switch_locale
+
+    def switch_locale(&action)
+        I18n.with_locale(locale_from_header, &action)
+    end
+
+    private
+
+    def locale_from_header
+        begin
+            request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+        rescue NoMethodError
+            'en' # Default value on error
+        end
+    end
 end
